0bdd20ccb047d85c9b3fd8c11887a173699a9dff
erickosma <erickosma@gmail.com>
2018-06-24 17:25:13 -0300

    repositorys

---
+++ src/main/java/com/zoy/pontoapi/api/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.zoy.pontoapi.api.enums;
+
+public enum PerfilEnum {
+    ROLE_ADMIN,
+    ROLE_USUARIO;
+}
---
+++ src/main/java/com/zoy/pontoapi/api/repositories/EmpresaRepository.java
@@ -0,0 +1,13 @@
+package com.zoy.pontoapi.api.repositories;
+
+import com.zoy.pontoapi.api.entities.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
